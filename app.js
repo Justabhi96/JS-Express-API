d1fc2dc204eff7e065355b68b9421ef07ab530b2
Abhishek Kushwaha <38038536+Justabhi96@users.noreply.github.com>
2020-09-21 14:44:00 +0530

    Added more endpoints

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const app = express()
 const port = process.env.PORT || 80
 
 app.get('', (req, res) => {
+    res.send("A Simple API for time.")
+})
+
+
+app.get('/today', (req, res) => {
     var today = new Date()
 
     res.send(today)
-    
+})
+
+
+app.get('/tommorow', (req, res) => {
+    var tommorow = new Date();
+    tommorow.setDate(tommorow.getDate() + 1);
+
+    res.send(tommorow)
 })
 
 app.listen(port)
